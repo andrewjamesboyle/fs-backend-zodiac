a12099c3f70f352b8e458250a0fd70c7c4e9b86f
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-11-01 13:27:29 -0700

    first test written and failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,21 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { zodiac } = require('../lib/zodiac-data');
+
 describe('example test - you should probably update me', () => {
   it('home route should return hello world', async () => {
     const resp = await request(app).get('/');
     expect(resp.text).toEqual('hello world!');
+  });
+});
+
+describe('zodiac list test', () => {
+  it('home route should return zodiac list', async () => {
+    const resp = await request(app).get('/zodiac');
+    const expected = zodiac.map((sign) => {
+      return { id: sign.id, name: sign.name };
+    });
+    expect(resp.text).toEqual(expected);
   });
 });
