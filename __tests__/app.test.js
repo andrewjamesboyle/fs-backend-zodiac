f5630fa838919454df82873dd8db974403d3fbd0
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-11-01 14:47:42 -0700

    zodiac detail test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,6 @@ const app = require('../lib/app');
 
 const { zodiac } = require('../lib/zodiac-data');
 
-// describe('example test - you should probably update me', () => {
-//   it('home route should return hello world', async () => {
-//     const resp = await request(app).get('/');
-//     expect(resp.text).toEqual('hello world!');
-//   });
-// });
-
 describe('zodiac list test', () => {
   it('home route should return zodiac list', async () => {
     const resp = await request(app).get('/zodiac');
